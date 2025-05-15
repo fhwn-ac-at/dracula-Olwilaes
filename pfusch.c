809e0bbb40aafba6dff2b34bf43cd01cd6a0189d
Oliver Willesberger <oliver@Olivers-MacBook-Air-2.local>
2025-05-15 11:23:02 +0200

    added adjacency matrix generator

--- pfusch.c
+++ pfusch.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
 
+typedef struct Edge {
+    int from;
+    int to;
+    int weight;
+} Edge;
+
 int main(void) {
-    printf("Hello, World!\n");
+    int verticeamount = 4;
+    Edge edges[] = {
+        {0, 1, 1}, {0, 2, 1}, {0, 3, 1},
+        {1, 0, 1}, {1, 2, 1}, {1, 3, 1},
+        {2, 0, 1}, {2, 1, 1}, {2, 3, 1},
+        {3, 0, 1}, {3, 1, 1}, {3, 2, 1}
+    };
+    int edgecount = sizeof(edges) / sizeof(edges[0]);
+
+    // Header
+    printf("+  ");
+    for (int i = 0; i < verticeamount; i++) {
+        printf("%d  ", i);
+    }
+    printf("\n");
+
+    // Separator
+    printf("  +");
+    for (int i = 0; i <= verticeamount; i++) {
+        printf("--");
+    }
+    printf("\n");
+
+    // Matrix
+    for (int j = 0; j < verticeamount; j++) {
+        printf("%d |", j);
+        for (int k = 0; k < verticeamount; k++) {
+            if (j == k) {
+                printf("0  ");
+            } else {
+                for (int e = 0; e < edgecount; e++) {
+                    if (edges[e].from == j && edges[e].to == k) {
+                        printf("%d  ", edges[e].weight);
+                        break;
+                    }
+                }
+            }
+        }
+        printf("\n");
+    }
 
     return 0;
 }
